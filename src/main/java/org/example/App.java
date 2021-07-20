8cbb2abd06b9eb6a9ebe43bc21e19cf71915d96e
Popa Vasile <popa.vasille90@gmail.com>
2021-07-20 15:13:49 +0300

    clase

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,41 @@
 package org.example;
-
+import java.util.Date;
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ){
+        Animal luck = new Animal();
+        luck.name = "Luck";
+        luck.age = 1;
+        luck.LevelOfHealth = 10;
+        luck.favoriteFood = "Royal Canin boabe";
+        luck.moodLevel = 9;
+        luck.feelingHungry = 1;
+        luck.preferredRecreationalActivity = "Play cu gaina";
+        
+        RecreationActivity playCuGaina = new RecreationActivity();
+        playCuGaina.name = "Play cu gaina";
+        
+        Adoption bebe = new Adoption();
+        bebe.name = "Bebe";
+        bebe.amountAvailable = 100;
+        
+        PetFood boabe = new PetFood();
+        boabe.name = "Royal Canin";
+        boabe.price = 15.23 ;
+        boabe.quantity = 9.50;
+        boabe.expirationDate = new Date(30,05,2022);
+        boabe.availabilityInStock = 23;
+        
+        Veterinarian drAnghel = new Veterinarian();
+        drAnghel.name = "Anghel Rusu";
+        drAnghel.specialization = "medicina generala";
+        
+        
+                
+        
     }
 }
